19f0b37db47c7983ded0dfa9fed89072e9425e5e
Егор <57827017+Egopoler@users.noreply.github.com>
2022-02-09 16:02:56 +0300

    8 с наборами
    ъеъ

--- avto8.py
+++ avto8.py
@@ -9,6 +9,11 @@ def d(x):
 
 
 def programm(nabor, first):
+    """
+    :param nabor: используемый алфавит
+    :param first: искомый набор
+    :return: номер набора
+    """
     t = list(map(d, list(itertools.product(nabor, repeat=len(first)))))
     return t.index(first) + 1
 
